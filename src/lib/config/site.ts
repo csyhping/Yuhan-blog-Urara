558e787d6ac2babfadb26ac5b6e6b6aa29fdecd9
Fyphia <38368516+csyhping@users.noreply.github.com>
2022-10-05 17:11:05 +0800

    Update site.ts

--- src/lib/config/site.ts
+++ src/lib/config/site.ts
@@ -3,15 +3,15 @@ import type { SiteConfig } from '$lib/types/site'
 export const site: SiteConfig = {
   protocol: 'https://',
   domain: import.meta.env.URARA_SITE_DOMAIN ?? 'urara-demo.netlify.app',
-  title: 'Urara',
-  subtitle: 'Sweet & Powerful SvelteKit Blog Template',
+  title: 'Home',
+  subtitle: 'Where am I',
   lang: 'en-US',
   description: 'Powered by SvelteKit/Urara',
   author: {
-    name: 'John Doe',
-    avatar: '/assets/maskable@192.png',
+    name: 'Yuhan Ping',
+    avatar: '/assets/avatar.png',
     status: '🌸',
-    bio: 'lorem ipsum dolor sit amet, consectetur adipiscing elit.'
+    bio: 'U have me at hello.'
   },
   themeColor: '#3D4451'
 }
