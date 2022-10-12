7fe0218a47c35c7496400ea8f13cb3fe50a031fa
Fyphia <38368516+csyhping@users.noreply.github.com>
2022-10-12 11:11:24 +0800

    Update site.ts

--- src/lib/config/site.ts
+++ src/lib/config/site.ts
@@ -3,8 +3,8 @@ import type { SiteConfig } from '$lib/types/site'
 export const site: SiteConfig = {
   protocol: 'https://',
   domain: import.meta.env.URARA_SITE_DOMAIN ?? 'urara-demo.netlify.app',
-  title: 'Home',
-  subtitle: 'Where am I',
+  title: 'csyhping',
+  subtitle: 'U have me at hello.',
   lang: 'en-US',
   description: 'Powered by SvelteKit/Urara',
   author: {
@@ -12,6 +12,13 @@ export const site: SiteConfig = {
     avatar: '/assets/avatar.png',
     status: '🌸',
     bio: 'The University of Hong Kong <br> U have me at hello.'
+  metadata: [
+      {
+        // text: 'github',
+        icon: 'i-simple-icons-github',
+        link: 'https://github.com/csyhping'
+      },
+    ]
   },
   themeColor: '#3D4451'
 }
